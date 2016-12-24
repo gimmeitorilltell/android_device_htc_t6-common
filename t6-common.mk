4171f40fe54c9da34435e24da1411e7b04982313
Christopher R. Palmer <crpalmer@gmail.com>
2016-12-24 11:36:06 -0800

    msm8960-common: Use shim for extra RIL symbol
    Change-Id: I2ce4a59c4d5fc29032740b5b841f77f8feda1e95

--- t6-common.mk
+++ t6-common.mk
@@ -143,6 +143,10 @@ PRODUCT_PACKAGES += \
     init.target.sh \
     ueventd.qcom.rc
 
+# RIL
+PRODUCT_PACKAGES += \
+    libshim_ril
+
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermald.conf:system/etc/thermald.conf
