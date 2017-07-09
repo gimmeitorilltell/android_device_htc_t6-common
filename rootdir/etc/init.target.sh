267b2162eeb70e5fa65b6fb743a5cdd642f02f83
TARKZiM <tom8476oo@gmail.com>
2017-07-09 16:48:25 +0800

    t6-common: unified device tree
    Change-Id: I46f8be1f9f931138b767db04b6dc27953580853d

---
+++ rootdir/etc/init.target.sh
@@ -0,0 +1,14 @@
+#!/system/bin/sh
+
+buildvariant=`getprop ro.boot.mid`
+case "$buildvariant" in
+    "0P3P10000" | "0P3P70000")
+        /system/vendor/bin/qcks -1 modem_st1 -2 modem_st2 -3 radio_config -4 cdma_record -i /firmware/mdm/image/
+        ;;
+    "0P3P52000" | "0P3P51000")
+        /system/vendor/bin/qcks -1 modem_st1 -2 modem_st2 -3 radio_config -4 cdma_record -i /firmware/mdm/image/ -u
+        ;;
+    *)
+        /system/vendor/bin/qcks -1 modem_st1 -2 modem_st2 -3 radio_config -4 cdma_record -i /firmware/mdm/image/ -u
+        ;;
+esac
