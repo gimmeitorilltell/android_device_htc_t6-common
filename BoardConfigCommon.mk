f492b0624e248f6fe374bbc04da78cee77987d4c
David Hays <dhays90@gmail.com>
2018-01-19 10:21:39 -0600

    t6-common: Build permissive
    Change-Id: I825dc727ce74d65ea2adb1ce37cc4989846c898d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -34,7 +34,7 @@ TARGET_SPECIFIC_HEADER_PATH += device/htc/t6-common/include
 # Kernel
 BOARD_KERNEL_BASE := 0x80600000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=qcom user_debug=23
+BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=qcom user_debug=23 androidboot.selinux=permissive
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01800000
 TARGET_KERNEL_CONFIG := t6_defconfig
 TARGET_KERNEL_SOURCE := kernel/htc/msm8960
@@ -95,9 +95,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 BOARD_RIL_CLASS := ../../../$(LOCAL_PATH)/ril
 
 # SELinux
--include device/qcom/sepolicy/sepolicy.mk
-
-BOARD_SEPOLICY_DIRS += device/htc/t6-common/sepolicy
+#BOARD_SEPOLICY_DIRS += device/htc/t6-common/sepolicy
 
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
